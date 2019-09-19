faf49359cf9405ab364202a423c76d77be21f51b
Maksim <maks_gr@rambler.ru>
2019-09-19 09:41:18 +0400

    Initial Commit

---
+++ Friends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Friends
+//
+//  Created by Maksim Grebenozhko on 19/09/2019.
+//  Copyright © 2019 Maksim Grebenozhko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
